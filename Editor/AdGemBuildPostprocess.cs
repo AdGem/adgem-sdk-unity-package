bbd6581101e53d5912feaf91d1eba630a0738428
Andriy Marshalek <amarshalek@ninevastudios.com>
2024-05-27 16:03:05 +0300

    add dependencies, build post processor script

---
+++ Editor/AdGemBuildPostprocess.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor.Android;
+
+public class AdGemBuildPostprocess : IPostGenerateGradleAndroidProject
+{
+	public int callbackOrder { get; } = 117;
+
+	public void OnPostGenerateGradleAndroidProject(string path)
+	{
+	}
+}
