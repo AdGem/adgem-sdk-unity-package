8d8d23d7691d73272eb06e7b94c85809e2c99dad
Dakota Washok <dakotawashok@users.noreply.github.com>
2024-11-05 12:01:19 -0600

    feat: check for updates in the android sdk repo and update our dependencies file if there is

---
+++ bin/check-for-updates.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const xml2js = require('xml2js');
+
+// Read the GitHub token from the environment variable
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+if (!GITHUB_TOKEN) {
+  console.error('Error: GitHub token is not set in the environment variables.');
+  process.exit(1);
+}
+
+const configFilePath = path.join(__dirname, '../Editor/Android/AdGemDependencies.xml');
+
+async function checkAdGemSdkUpdates() {
+  // Read the current version from AdGemDependencies.xml
+  const xmlContent = fs.readFileSync(configFilePath, 'utf8');
+  const parser = new xml2js.Parser();
+  const xml = await parser.parseStringPromise(xmlContent);
+  const currentVersion = xml.dependencies.androidPackages[0].androidPackage[0].$.spec.split(':').pop();
+
+  // Get the latest version from the AdGem GitHub releases
+  const response = await axios.get('https://api.github.com/repos/AdGem/Android-SDK/releases/latest', {
+    headers: {
+      'Accept': 'application/vnd.github+json',
+      'Authorization': `Bearer ${GITHUB_TOKEN}`,
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+
+  const latestVersion = response.data.tag_name;
+
+  if (latestVersion !== currentVersion) {
+    // Update the AdGemDependencies.xml with the new version
+    xml.dependencies.androidPackages[0].androidPackage[0].$.spec = `com.adgem:adgem-android:${latestVersion}`;
+    const builder = new xml2js.Builder();
+    const updatedXmlContent = builder.buildObject(xml);
+    fs.writeFileSync(configFilePath, updatedXmlContent);
+  }
+}
+
+checkAdGemSdkUpdates();
