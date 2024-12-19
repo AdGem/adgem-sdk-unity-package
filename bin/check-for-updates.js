cda88b88bdde51e5568a2ecc8f320f943f6f6d49
Dakota Washok <dakotawashok@users.noreply.github.com>
2024-12-18 19:01:17 -0600

    feat: add functionality to update self-owned repository release version references in different parts of our code

--- bin/check-for-updates.js
+++ bin/check-for-updates.js
@@ -5,22 +5,20 @@ const xml2js = require('xml2js');
 
 // Read the GitHub token from the environment variable
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const CHECK_TYPE = process.env.CHECK_TYPE || 'external'; // Default to 'external' if not set
 
 if (!GITHUB_TOKEN) {
   console.error('Error: GitHub token is not set in the environment variables.');
   process.exit(1);
 }
 
-const configFilePath = path.join(__dirname, '../Editor/Android/AdGemDependencies.xml');
-
-async function checkAdGemSdkUpdates() {
-  // Read the current version from AdGemDependencies.xml
+async function checkExternalUpdates() {
+  const configFilePath = path.join(__dirname, '../Editor/Android/AdGemDependencies.xml');
   const xmlContent = fs.readFileSync(configFilePath, 'utf8');
   const parser = new xml2js.Parser();
   const xml = await parser.parseStringPromise(xmlContent);
   const currentVersion = xml.dependencies.androidPackages[0].androidPackage[0].$.spec.split(':').pop();
 
-  // Get the latest version from the AdGem GitHub releases
   const response = await axios.get('https://api.github.com/repos/AdGem/Android-SDK/releases/latest', {
     headers: {
       'Accept': 'application/vnd.github+json',
@@ -33,7 +31,6 @@ async function checkAdGemSdkUpdates() {
   latestVersion = latestVersion.replace(/^v/, ''); // Remove leading 'v'
 
   if (latestVersion !== currentVersion) {
-    // Update the AdGemDependencies.xml with the new version
     xml.dependencies.androidPackages[0].androidPackage[0].$.spec = `com.adgem:adgem-android:${latestVersion}`;
     const builder = new xml2js.Builder();
     const updatedXmlContent = builder.buildObject(xml);
@@ -43,4 +40,39 @@ async function checkAdGemSdkUpdates() {
   console.log(latestVersion);
 }
 
-checkAdGemSdkUpdates();
+async function checkOwnRepoUpdates() {
+  const packageJsonPath = path.join(__dirname, '../package.json');
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+  const currentVersion = packageJson.version;
+
+  const response = await axios.get('https://api.github.com/repos/AdGem/adgem-sdk-unity-package/releases/latest', {
+    headers: {
+      'Accept': 'application/vnd.github+json',
+      'Authorization': `Bearer ${GITHUB_TOKEN}`,
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+
+  let latestVersion = response.data.tag_name;
+  latestVersion = latestVersion.replace(/^v/, ''); // Remove leading 'v'
+
+  if (latestVersion !== currentVersion) {
+    packageJson.version = latestVersion;
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  }
+
+  console.log(latestVersion);
+}
+
+console.log('Checking for updates...');
+console.log('Github Token:', GITHUB_TOKEN);
+console.log('Check Type:', CHECK_TYPE);
+
+if (CHECK_TYPE === 'external') {
+  checkExternalUpdates();
+} else if (CHECK_TYPE === 'self') {
+  checkOwnRepoUpdates();
+} else {
+  console.error('Error: Invalid check type. Use "external" or "self".'); // We'll add separate checks for android vs ios later
+  process.exit(1);
+}
